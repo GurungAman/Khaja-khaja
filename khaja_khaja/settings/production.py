cb2cab62f851c757f7ab45fdaf2ee3326504c032
Aman Gurung <amenag30@gmail.com>
2021-10-08 15:37:31 +0545

    variable name change

--- khaja_khaja/settings/production.py
+++ khaja_khaja/settings/production.py
@@ -9,8 +9,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'khaja_khaja',
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': 'db',
         'PORT': '5432',
     }
